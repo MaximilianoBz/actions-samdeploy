f7359285687b238d2169d17fce1b76891295d1d4
Maximiliano Baez <maximilianombaez@gmail.com>
2023-02-07 17:15:33 -0300

    Added AWS variables
    Signed-off-by: Maximiliano Baez <maximiliano.baez@peperina.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
-#
-#GITHUB_TAG=${GITHUB_REF/refs\/tags\//}
-#AWS_REGION=${AWS_REGION:-$AWS_DEFAULT_REGION}
-#
-#SAM_SRC=${1}
-#SAM_TEMPLATE=${2}
-#S3_BUCKET=${3}
-#S3_BUCKET_PREFIX=${4}
-#
-#if [ ! -z "$5" ]
-#then
-#  SAM_VERSION=$5
-#else
-#  SAM_VERSION=$GITHUB_TAG
-#fi
-#
-#cd $SAM_SRC
 
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+    echo AWS Access Key ID invalid
+    exit 1
+fi
+
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+    echo AWS Secret Access Key invalid
+    exit 1
+fi
+
+if [[ -z "$AWS_REGION" ]]; then
+    echo AWS Region invalid
+    exit 1
+fi
+
+# if exist folder delete
+if [ -d ~/.aws ]; then
+    rm -rf ~/.aws
+fi
+
+mkdir ~/.aws
+touch ~/.aws/credentials
+touch ~/.aws/config
+
+echo "[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+region = $AWS_REGION" > ~/.aws/credentials
+
+echo "[default]
+output = text
+region = $AWS_REGION" > ~/.aws/config
+
+## Deploy
 sam deploy --config-env $SAM_ENV
 
 #sam package --region $AWS_REGION --s3-bucket $S3_BUCKET --s3-prefix $S3_BUCKET_PREFIX --template-file $SAM_TEMPLATE --output-template-file packaged.yaml
